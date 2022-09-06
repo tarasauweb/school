d5899eb954440fc83f14172541086606c4a26876
Artur Tarasau <tarasov.a.web@gmail.com>
2022-09-06 14:58:05 +0300

    fix: main fon

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('load' , ()=>{
     const sizeWindowWidth = document.documentElement.clientWidth
     const sizeWindowHeigth = document.documentElement.clientHeight
-    const headerFon = document.querySelector('.header-fone')
-    if(sizeWindowWidth>1440){
+    const imgResponsive = document.querySelector('.img-responsive')
+    const imgResponsiveClientHeight = imgResponsive.clientHeight
+    const header = document.querySelector('.header')
+    if(sizeWindowWidth<576){
         
-        headerFon.style.right = `0%`
+        header.style.height = imgResponsiveClientHeight - 4 + 'px'
     }
     
     console.log(sizeWindowHeigth,sizeWindowWidth)
