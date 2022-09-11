cb09efe69109cf53fd3d93a0a2399d95b966bd3e
Artur Tarasau <tarasov.a.web@gmail.com>
2022-09-11 21:30:11 +0300

    change link

--- js/script.js
+++ js/script.js
@@ -125,7 +125,8 @@ window.addEventListener("load", () => {
   }
 
   function openVideo() {
-    const video = `<iframe width="560" height="315" src="https://www.youtube.com/watch?v=tXB3XXBQm_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="postVideo" enablejsapi=1></iframe>`;
+    const video = `<iframe width="560" height="315" src="https://www.youtube.com/embed/tXB3XXBQm_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> enablejsapi=1`;
+    // enablejsapi=1
     const play = document.querySelector(".offer__btn");
     const overlay = document.querySelector(".overlay-video");
     play.addEventListener("click", () => {
