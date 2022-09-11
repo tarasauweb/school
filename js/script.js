895c8ea379367aa4d95b302078bc5e39373c35fe
Artur Tarasau <89244557+tarasauweb@users.noreply.github.com>
2022-09-11 14:24:11 +0300

    update link for youtube

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ window.addEventListener("load", () => {
   }
 
   function openVideo() {
-    const video = `<iframe width="560" height="315" src="https://www.youtube.com/embed/lLn2i1GEtQ0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="postVideo" enablejsapi=1></iframe>`;
+    const video = `<iframe width="560" height="315" src="https://www.youtube.com/watch?v=tXB3XXBQm_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="postVideo" enablejsapi=1></iframe>`;
     const play = document.querySelector(".offer__btn");
     const overlay = document.querySelector(".overlay-video");
     play.addEventListener("click", () => {
